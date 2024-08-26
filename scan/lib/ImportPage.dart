06618f96acb9238577a861d27cef8d65038886ea
rihabisit <130654278+rihabisit@users.noreply.github.com>
2024-08-26 12:59:15 +0100

    Update ImportPage.dart

--- scan/lib/ImportPage.dart
+++ scan/lib/ImportPage.dart
@@ -1,33 +1,16 @@
-// Cette interface Flutter permet à l'utilisateur d'importer des fichiers images ou PDF, de les recadrer, de détecter la langue du texte qu'ils contiennent, et d'extraire ce texte pour un traitement ultérieur.
-// Le texte extrait peut être affiché, analysé ou utilisé pour d'autres applications, comme la traduction ou la transcription.
-// L'interface prend en charge les images prises avec la caméra, sélectionnées depuis la galerie, ou importées en tant que fichiers PDF.
-// Elle utilise Google ML Kit pour la reconnaissance de texte et le package flutter_langdetect pour la détection de la langue.
-// Attention :supprimer l'utilisation de google ML Kit car l'app est liée avec le model pour detecter le lang
-// lorsque appuie sur le button import pdf
-
-
 import 'dart:io';
-import 'dart:math';
-import 'package:camera/camera.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-//import 'package:google_ml_kit/google_ml_kit.dart';
-//import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'Translator.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-import 'package:flutter/widgets.dart';
-import 'ExtractTextPage.dart';
 import 'package:image_cropper/image_cropper.dart';
-
-// recommend to import 'as langdetect' because this package shows a simple function name 'detect'
+import 'package:http/http.dart' as http;
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:flutter_langdetect/flutter_langdetect.dart' as langdetect;
+import 'ExtractTextPage.dart';
+import 'Translator.dart'; // Ensure this import is correct
 
 class ImportPage extends StatefulWidget {
   final File file;
-
 
   ImportPage({required this.file});
 
@@ -38,45 +21,15 @@ class ImportPage extends StatefulWidget {
 class _ImportPageState extends State<ImportPage> {
   String _detectedLanguage = '';
   late File _croppedFile;
-  late TextRecognizer textRecognizer;
+  String results = "";
+  bool _isPdf = true;
 
   @override
   void initState() {
     super.initState();
     _croppedFile = widget.file;
-    _detectLanguage(widget.file);
+    _isPdf = widget.file.path.endsWith('.pdf');
     _cropImage(_croppedFile);
-    textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
-    //_detectLanguage(widget.file);
-    doTextRecognition();
-  }
-
-  String results = "";
-
-  doTextRecognition() async {
-    InputImage inputImage = InputImage.fromFile(this.widget.file);
-    final RecognizedText recognizedText = await textRecognizer.processImage(
-        inputImage);
-
-    results = recognizedText.text;
-    print(results);
-    setState(() {
-      results;
-    });
-
-    for (TextBlock block in recognizedText.blocks) {
-      final Rect rect = block.boundingBox;
-      final List<Point<int>> cornerPoints = block.cornerPoints;
-      final String text = block.text;
-      final List<String> languages = block.recognizedLanguages;
-
-      for (TextLine line in block.lines) {
-        // Same getters as TextBlock
-        for (TextElement element in line.elements) {
-          // Same getters as TextBlock
-        }
-      }
-    }
   }
 
   Future<void> _cropImage(File file) async {
@@ -89,49 +42,79 @@ class _ImportPageState extends State<ImportPage> {
         CropAspectRatioPreset.ratio4x3,
         CropAspectRatioPreset.ratio16x9,
       ],
-      uiSettings:[ AndroidUiSettings(
-        toolbarTitle: 'Recadrer l\'image',
-        backgroundColor: Colors.white,
-        toolbarWidgetColor: Colors.lightGreen[700],
-        initAspectRatio: CropAspectRatioPreset.original,
-        lockAspectRatio: false,
-      ),
-      IOSUiSettings(
-        minimumAspectRatio: 1.0,
-      ),]
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: 'Recadrer l\'image',
+          backgroundColor: Colors.white,
+          toolbarWidgetColor: Colors.purple.shade100,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false,
+        ),
+        IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ),
+      ],
     );
 
     if (cropped != null) {
       setState(() {
         _croppedFile = File(cropped.path);
-
       });
-      _detectLanguage(_croppedFile);
+      await _uploadFile(_croppedFile, _isPdf);
     }
   }
-  String detectedLanguage ='';
-  void _detectLanguage(XFile) async {
+
+  Future<void> _uploadFile(File file, bool isPdf) async {
+    final uri = Uri.parse('http://192.168.1.65:5000/${isPdf ? 'ocr_pdf' : 'ocr'}');
+    var request = http.MultipartRequest('POST', uri);
+
+    request.files.add(await http.MultipartFile.fromPath(
+      isPdf ? 'pdf' : 'image',
+      file.path,
+    ));
+
+    request.fields['language[]'] = 'eng'; // You can add more languages here
+
+    print('Uploading file: ${file.path} to $uri'); // Debugging line
+
+    try {
+      var response = await request.send();
+      print('Response status code: ${response.statusCode}'); // Debugging line
+
+      if (response.statusCode == 200) {
+        final responseData = await response.stream.bytesToString();
+        print('Response data: $responseData'); // Debugging line
+
+        final Map<String, dynamic> responseJson = json.decode(responseData);
+        setState(() {
+          results = responseJson['eng']['text'] ?? '';
+          _detectLanguage(results);
+        });
+      } else {
+        print('Failed to upload file. Status code: ${response.statusCode}'); // Debugging line
+      }
+    } catch (e) {
+      print('Error uploading file: $e'); // Debugging line
+    }
+  }
+
+  void _detectLanguage(String text) async {
     WidgetsFlutterBinding.ensureInitialized();
-
-    await langdetect.initLangDetect();  // This is needed once in your application after ensureInitialized()
-
-    String text = results;
+    await langdetect.initLangDetect(); // Initialize language detection
 
     final language = langdetect.detect(text);
-   // print('Detected language: $language'); // -> "en"
-    detectedLanguage = language;
-    final probs = langdetect.detectLangs(text);
-    for (final p in probs) {
-      print("Language: ${p.lang}");  // -> "en"
-      print("Probability: ${p.prob}");  // -> 0.9999964132193504
-    }
+    setState(() {
+      _detectedLanguage = language;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple.shade50,
       appBar: AppBar(
-        title: Text('Traitement', style: GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.bold)),
+        title: Text('Traitment', style: TextStyle(fontSize: 24)),
+        backgroundColor: Colors.purple.shade100,
         centerTitle: true,
       ),
       body: SingleChildScrollView(
@@ -139,92 +122,51 @@ class _ImportPageState extends State<ImportPage> {
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: widget.file.path.endsWith('.pdf')
-                  ? Text('Fichier PDF : ${_croppedFile}')
-                  : Image.file(_croppedFile)
+              child: _isPdf
+                  ? Container(
+                height: 600,
+                child: PDFView(
+                  filePath: _croppedFile.path,
+                ),
+              )
+                  : Image.file(_croppedFile),
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Appel de la fonction pour extraire le texte du fichier
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> ExtractTextPage(file: _croppedFile)),
-                );
+              onPressed: () async {
+                if (_croppedFile != null) {
+                  await _uploadFile(_croppedFile, _isPdf);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ExtractTextPage(
+                        file: _croppedFile,
+                        extractedText: results, // Pass the extracted text here
+                        languageCode: _detectedLanguage.isNotEmpty ? _detectedLanguage : 'eng',
+                      ),
+                    ),
+                  );
+                } else {
+                  print('No file selected.');
+                }
               },
               style: TextButton.styleFrom(
-                backgroundColor: Colors.lightGreen[700],
+                backgroundColor: Colors.purple.shade100,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
                 padding: EdgeInsets.symmetric(vertical: 14, horizontal: 28),
               ),
-              child: Text('Extraire Tx.', style: TextStyle(color: Colors.white)),
+              child: Text('Extracted Text', style: TextStyle(color: Colors.white)),
             ),
+
             SizedBox(height: 20),
-            Text(
-              'Langue détectée : $detectedLanguage',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
+            // Text(
+            //   'Langue détectée : $_detectedLanguage',
+            //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            // ),
           ],
         ),
-      ),
-    );
-  }
-
-  void _navigateToPDFViewerPage(File file) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => PDFViewerPage(file: file),
-      ),
-    );
-  }
-}
-
-
-
-
-class PDFViewerPage extends StatelessWidget {
-  final File file;
-
-  PDFViewerPage({required this.file});
-
-  @override
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('PDF Viewer'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: PDFView(
-              filePath: file.path,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> ImportPage(file: file)),
-                  );
-
-
-                },
-                child: Text('Extraire TXT'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> TranslatorScreen()),
-                  );
-
-                },
-                child: Text('Transcription'),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
