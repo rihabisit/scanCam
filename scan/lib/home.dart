dd95a5f07b10c6e3e855a83ee3b0f4e11224f8f2
rihabisit <130654278+rihabisit@users.noreply.github.com>
2024-08-26 12:58:53 +0100

    Update home.dart

--- scan/lib/home.dart
+++ scan/lib/home.dart
@@ -1,28 +1,19 @@
-// Cette interface Flutter permet à l'utilisateur de scanner, importer, et gérer des fichiers PDF ou des images.
-// Elle offre des fonctionnalités telles que la capture d'image via la caméra ou la galerie, la sélection de fichiers PDF, et la gestion des fichiers importés (renommer, supprimer).
-// L'utilisateur peut également visualiser les fichiers PDF, extraire du texte des PDF, et accéder à un outil de transcription.
-
 import 'dart:io';
-
-import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:scan/ExtractTextPage.dart';
-import 'Translator.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'ImportPage.dart'; // Make sure to import necessary files
 
-import 'ImportPage.dart';
-
-class HomeScreen extends StatefulWidget {
+class MyHome extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _MyHomeState createState() => _MyHomeState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  //File? _selectedImage;
-  //File? _selectedPdf;
+class _MyHomeState extends State<MyHome> {
+  final ImagePicker _picker = ImagePicker();
   File? _selectedFile;
   List<FileSystemEntity> savedFiles = [];
 
@@ -44,267 +35,84 @@ class _HomeScreenState extends State<HomeScreen> {
     await fetchSavedFiles();
   }
 
-
-  Future<void> renameFile(File file) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final newNameController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Rename File'),
-        content: TextField(
-          controller: newNameController,
-          decoration: InputDecoration(hintText: 'Enter new file name'),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () async {
-              final newName = newNameController.text;
-              if (newName.isNotEmpty) {
-                final newFilePath = '${directory.path}/$newName.pdf';
-                await file.rename(newFilePath);
-                fetchSavedFiles();
-                Navigator.of(context).pop();
-              }
-            },
-            child: Text('Rename'),
-          ),
-        ],
-      ),
-    );
+  Future<void> _openCamera() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    if (photo != null) {
+      setState(() {
+        _selectedFile = File(photo.path);
+        _navigateToImportPage();
+      });
+    }
   }
 
-  Future<void> deleteFile(File file) async {
+  Future<void> _openGallery() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _selectedFile = File(image.path);
+        _navigateToImportPage();
+      });
+    }
+  }
+
+  Future<void> _openFilePicker() async {
+    final result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ['pdf']);
+    if (result != null) {
+      setState(() {
+        _selectedFile = File(result.files.single.path!);
+        _navigateToPDFViewerPage(_selectedFile!);
+      });
+    }
+  }
+
+  Future<void> _renameFile(File file, String newName) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final newFilePath = '${directory.path}/$newName.pdf';
+    await file.rename(newFilePath);
+    fetchSavedFiles();
+  }
+
+  Future<void> _deleteFile(File file) async {
     await file.delete();
     fetchSavedFiles();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('File deleted')),
-    );
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('File deleted')));
   }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(10),
+
+  Future<void> _showRenameDialog(File file) async {
+    TextEditingController _controller = TextEditingController(text: file.path.split('/').last);
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text('Rename File'),
+          content: TextField(
+            controller: _controller,
+            decoration: InputDecoration(hintText: 'Enter new file name'),
           ),
-        ),
-        elevation: 4,
-       //backgroundColor: Colors.transparent,
-        // backgroundColor: Colors.tealAccent[400],
-        title: Text(
-          'Home',
-          style: GoogleFonts.roboto(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-    body: RefreshIndicator(
-      onRefresh: _refresh,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child:
-              Image.asset(
-                'assets/images/200w.gif',
-                width: 350,
-                height: 250,
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
               onPressed: () {
-                _pickImageFromCamera();
-                // Action to take when the first button is pressed
-                // (e.g., open camera)
+                Navigator.of(dialogContext).pop();
               },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.lightGreen[700],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // Bordure arrondie
-                ),// Définir la couleur de fond du bouton
-                padding: EdgeInsets.symmetric(vertical: 14, horizontal: 40),
-              ),
-              child: Text('Scan',
-                style: TextStyle(
-                    color: Colors.white
-                ),),
             ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-        
-                ElevatedButton(
-        
-                  onPressed: () {
-                    _pickImageFromGallery();
-                    // Action to take when the second button is pressed
-                    // (e.g., open gallery to select photo)
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.lightGreen[700], // Définir la couleur de fond du bouton
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10), // Bordure arrondie
-                    ),
-                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 28),
-                  ),
-                  child: Text('Import Photo',
-                  style: TextStyle(
-                    color: Colors.white
-                  ),
-                  ),
-        
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    _pickFilePdf();
-
-                    //_pickPdfFromGallery();
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.lightGreen[700],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10), // Bordure arrondie
-                    ),// Définir la couleur de fond du bouton
-                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 28),
-                  ),
-                  child: Text('Import PDF',
-                    style: TextStyle(
-                        color: Colors.white
-                    ),),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Recent:',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  'Placeholder for recent history',
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: savedFiles.length,
-              itemBuilder: (context, index) {
-                final file = savedFiles[index];
-                return Card(
-                  elevation: 4,
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: ListTile(
-                    leading: Icon(Icons.picture_as_pdf, color: Colors.green), // PDF icon
-
-                    title: Text(
-                      file.path.split('/').last,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () => renameFile(File(file.path)),
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () => deleteFile(File(file.path)),
-                        ),
-                      ],
-                    ),
-                    onTap: () {
-                      if (file.path.endsWith('.pdf')) {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => PDFViewerPage(file: File(file.path)),
-                          ),
-                        );
-                      }
-                      // Handle file tap if needed (e.g., open the file)
-                    },
-                  ),
-                );
+            TextButton(
+              child: Text('Rename'),
+              onPressed: () async {
+                String newName = _controller.text;
+                if (newName.isNotEmpty) {
+                  await _renameFile(file, newName);
+                }
+                Navigator.of(dialogContext).pop();
               },
             ),
           ],
-        ),
-      ),
-    ),
+        );
+      },
     );
   }
 
-
-  Future _pickImageFromGallery() async {
-    final pickedImage =
-    await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (pickedImage != null) {
-      setState(() {
-        _selectedFile = File(pickedImage.path);
-        _navigateToImportPage();
-      });
-    }
-  }
-  Future _pickImageFromCamera() async {
-    final pickedImage =
-    await ImagePicker().pickImage(source: ImageSource.camera);
-    if (pickedImage != null) {
-      setState(() {
-        _selectedFile = File(pickedImage.path);
-        _navigateToImportPage();
-      });
-    }
-  }
-  Future<void> _pickFilePdf() async {
-    try {
-      FilePickerResult? result = await FilePicker.platform.pickFiles(
-        type: FileType.custom,
-        allowedExtensions: ['pdf','doc', 'docx'],
-      );
-
-      if (result != null) {
-        setState(() {
-          _selectedFile = File(result.files.single.path!);
-          _navigateToPDFViewerPage(_selectedFile!);
-        });
-        // Vous pouvez maintenant utiliser _selectedFile
-        print('Fichier PDF sélectionné : ${_selectedFile!}');
-      } else {
-        // L'utilisateur a annulé le picker
-        print('File selection canceled');
-      }
-    } catch (e) {
-      print('An error occurred: $e');
-    }
-  }
   void _navigateToImportPage() {
     if (_selectedFile != null) {
       Navigator.of(context).push(
@@ -315,16 +123,127 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  void _navigateToPDFViewerPage(File file) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => PDFViewerPage(file: file),
+      ),
+    );
+  }
 
-void _navigateToPDFViewerPage(File file) {
-  Navigator.of(context).push(
-    MaterialPageRoute(
-      builder: (context) => PDFViewerPage(file: file),
-    ),
-  );
-}
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple.shade50,
+      appBar: AppBar(
+        title: Text('Home', style: GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.purple.shade100,
+        centerTitle: true,
+      ),
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/11201139.png'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: _buildButton(Icons.image, 'Gallery', Colors.pink.shade100, _openGallery),
+                  ),
+                  SizedBox(width: 10), // Add spacing between the two buttons
+                  Expanded(
+                    child: _buildButton(Icons.camera_alt, 'Camera', Colors.blue.shade100, _openCamera),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: _buildButton(Icons.folder, 'My PDF', Colors.cyan.shade100, _openFilePicker),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Recent Files', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                ),
+              ),
+              SizedBox(height: 10),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: savedFiles.length,
+                itemBuilder: (context, index) {
+                  final file = savedFiles[index];
+                  return Card(
+                    elevation: 4,
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: ListTile(
+                      leading: Icon(Icons.picture_as_pdf, color: Colors.red),
+                      title: Text(file.path.split('/').last, style: TextStyle(fontWeight: FontWeight.bold)),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () => _showRenameDialog(File(file.path)),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () => _deleteFile(File(file.path)),
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                        if (file.path.endsWith('.pdf')) {
+                          _navigateToPDFViewerPage(File(file.path));
+                        }
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
+  Widget _buildButton(IconData icon, String title, Color color, VoidCallback onPressed) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 40, color: Colors.white),
+              SizedBox(height: 10),
+              Text(title, style: TextStyle(fontSize: 16, color: Colors.white)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class PDFViewerPage extends StatelessWidget {
   final File file;
@@ -332,12 +251,11 @@ class PDFViewerPage extends StatelessWidget {
   PDFViewerPage({required this.file});
 
   @override
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('PDF Viewer'),
+        backgroundColor: Colors.purple.shade100,
       ),
       body: Column(
         children: [
@@ -350,21 +268,13 @@ class PDFViewerPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=> ImportPage(file: file)),
-                    );
-
-
-                },
-                child: Text('Extraire TXT'),
-              ),
-              ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> TranslatorScreen()),
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ImportPage(file: file)),
                   );
-
                 },
-                child: Text('Transcription'),
+                child: Text('Extracted Text'),
               ),
             ],
           ),
@@ -373,21 +283,3 @@ class PDFViewerPage extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
